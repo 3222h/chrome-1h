fca60822009b35d02033e12e558bbbbd423df3d3
3222h <394954a+++@gmail.com>
2025-05-09 22:17:22 +0530

    Update guit.sh

--- guit.sh
+++ guit.sh
@@ -62,7 +62,7 @@ if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&
 sleep 1
 clear
 docker run --restart always -d -p 3000:3000 --privileged --name nomashine --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP='5022' a35379/rdp:chrome
-
+clear
 
 
 
