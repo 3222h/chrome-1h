3a731382083456ebefd6c3d2d31a1541c1da7bf2
3222h <394954a+++@gmail.com>
2025-05-09 22:36:48 +0530

    Update guit.sh

--- guit.sh
+++ guit.sh
@@ -64,19 +64,7 @@ clear
 docker run --restart always -d -p 3000:3000 --privileged --name nomashine --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP='5022' a35379/rdp:chrome
 clear
 
-
-
-
-PSW_FILE="PSW"
-if [ -s "$PSW_FILE" ]; then
-    CRP=$(cat "$REGION_FILE")
-    echo "PASSWORD READED FROM FILE: $PSW."
-else
-    read -p "CHOOSE PASSWORD OF FOUR NUMBERS ( 1,2,3,4,5,6,7,8,9 ): " PSW
-    echo "$PSW" > "$PSW_FILE"
-    echo "PASSWORD SAVED TO FILE."
-fi
-docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:t
+docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='ubuntu' -e VNCP='ubuntu' a35379/rdp:t
 
 clear
 clear
