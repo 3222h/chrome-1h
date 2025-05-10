ffb9a92c9b4063f9d854ce0bda7306d133ac84a5
3222h <394954a+++@gmail.com>
2025-05-10 18:54:57 +0530

    Update t.sh

--- t.sh
+++ t.sh
@@ -7,4 +7,4 @@ docker stop nomashine1
 sleep 2
 docker rm nomashine1
 sleep 2
-docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='ubuntu' -e VNCP='ubuntu' a35379/rdp:t
+docker run --network nomashine1 --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='ubuntu' -e VNCP='ubuntu' a35379/rdp:t
