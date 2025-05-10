432064d60249f52883133578243299f1dfd30a1f
3222h <394954a+++@gmail.com>
2025-05-10 19:23:41 +0530

    Update guia.sh

--- guia.sh
+++ guia.sh
@@ -68,7 +68,7 @@ clear
 
 PSW_FILE="PSW"
 if [ -s "$PSW_FILE" ]; then
-    CRP=$(cat "$REGION_FILE")
+    CRP=$(cat "$PSW_FILE")
     echo "PASSWORD READED FROM FILE: $PSW."
 else
     read -p "CHOOSE PASSWORD OF FOUR NUMBERS ( 1,2,3,4,5,6,7,8,9 ): " PSW
