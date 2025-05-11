7bcfb9fd22dabe6ddc09a9837f796ee7b192811e
3222h <394954a+++@gmail.com>
2025-05-11 07:12:27 +0530

    Update t.sh

--- t.sh
+++ t.sh
@@ -4,7 +4,7 @@ stty quit ""
 stty susp undef
 
 docker stop nomashine1
-sleep 2
+sleep 1
 docker rm nomashine1
-sleep 2
+sleep 1
 docker run --network nomashine1 --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='ubuntu' -e VNCP='ubuntu' a35379/rdp:t
