60d0941ba0531ce865792d7451c226d310e728af
3222h <394954a+++@gmail.com>
2025-05-16 05:26:44 +0530

    Update l.sh

--- l.sh
+++ l.sh
@@ -5,9 +5,11 @@ stty susp undef
 
 docker stop nomashine1
 docker stop nomashine2
+docker stop nomashine3
 sleep 1
 docker rm nomashine1
 docker rm nomashine2
+docker rm nomashine3
 sleep 1
 
 PSW_FILE="PSW"
@@ -21,3 +23,4 @@ else
 fi
 docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
 docker run --restart always -d -p 3002:3000 --privileged --name nomashine2 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
+docker run --restart always -d -p 3003:3000 --privileged --name nomashine3 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
