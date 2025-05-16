ad069001850c0f8ddd3c15be7fa4b1656e57ec99
3222h <394954a+++@gmail.com>
2025-05-17 04:12:54 +0530

    Update guil1.sh

--- guil1.sh
+++ guil1.sh
@@ -19,7 +19,7 @@ clear
 curl -sSL -o t https://raw.githubusercontent.com/3222h/chrome-1h/main/l.sh
 
 clear
-docker run --restart always -d -p 3000:3000 --privileged --name nomashine --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP='5022' a35379/rdp:c
+docker run --restart always -d -p 3000:3000 --privileged --name nomashine --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP='5022' a35379/rdp:c1
 clear
 docker exec -it nomashine /bin/sh -c "git clone https://github.com/3222h/vs-terminal.git"
 docker exec -it nomashine /bin/sh -c "git clone https://github.com/3222h/vs-ter-01.git"
@@ -35,9 +35,9 @@ else
     echo "$PSW" > "$PSW_FILE"
     echo "PASSWORD SAVED TO FILE."
 fi
-docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
-docker run --restart always -d -p 3002:3000 --privileged --name nomashine2 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
-docker run --restart always -d -p 3003:3000 --privileged --name nomashine3 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c
+docker run --restart always -d -p 3001:3000 --privileged --name nomashine1 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c1
+docker run --restart always -d -p 3002:3000 --privileged --name nomashine2 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c1
+docker run --restart always -d -p 3003:3000 --privileged --name nomashine3 --cap-add=SYS_PTRACE --shm-size=7g -e USERP='5022' -e VNCP="$PSW" a35379/rdp:c1
 clear
 
 # Start Cloudflared tunnels for each port
